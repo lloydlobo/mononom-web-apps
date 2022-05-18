d70772e7fba0e7b87cd052e4a7752433f69f5753
Lloyd Lobo <lloyd@lloydlobo.com>
2022-05-18 23:01:38 +0530

    test: add basic unit tests
    ci: add types "jest" to tsconfig to serve
    
    	modified:   apps/nnwml/self-driving-car/tsconfig.app.json
    
    ci: add types "jest" to tsconfig to serve
    
    	modified:   apps/nnwml/self-driving-car/tsconfig.app.json
    
    chore: add codeSpell words to vscode settings
    
    ci: update commitlintrc.json with new chore type
    
    refactor: add types and add test for main.ts

--- apps/nnwml/self-driving-car/src/main.ts
+++ apps/nnwml/self-driving-car/src/main.ts
@@ -1,22 +1,26 @@
 import './app/app.element.ts';
 import Car from './app/features/car';
 
-const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+export const canvas: HTMLCanvasElement = document.getElementById(
+  'myCanvas'
+) as HTMLCanvasElement;
 
-canvas.width = 200;
+console.log(typeof canvas);
+
+(canvas as HTMLCanvasElement).width = 200 as number;
 
 // a drawing context is a way to draw on a canvas
-const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
+export const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
 
 // car dimensions in pixels (x,y,width,height) 0,0 is top left corner
-const car: Car = new Car(100, 100, 30, 50); /* now define the Car */
+export const car: Car = new Car(100, 100, 30, 50); /* now define the Car */
 
 // add car to DOM (in the canvas)
 car.draw(ctx);
 
 animate();
 
-function animate() {
+export function animate() {
   car.update();
   /* transferred from global:makes it full height */
   canvas.height = window.innerHeight;
---
+++ apps/nnwml/self-driving-car/src/test-setup.ts
@@ -0,0 +1,39 @@
+// import { animate, car, ctx, canvas } from './main';
+import Car from './app/features/car';
+import Controls from './app/features/controls';
+
+describe('Car Controls', () => {
+  const controls = new Controls();
+  let car: Car;
+
+  beforeEach(() => {
+    car = new Car(100, 100, 30, 50);
+    car.update();
+  });
+
+  it('should have a speed of 0', () => {
+    expect(car.speed).toBe(0);
+  });
+
+  it('control car forward', () => {
+    controls.forward = true;
+  });
+
+  it('control car reverse', () => {
+    controls.reverse = true;
+  });
+
+  it('control car left', () => {
+    controls.left = true;
+  });
+
+  it('control car right', () => {
+    controls.right = true;
+  });
+});
+
+// describe(`canvas`, () => {
+//   it('should have a width of 200', () => {
+//     expect(canvas.width).toBe(200);
+//   });
+// });
