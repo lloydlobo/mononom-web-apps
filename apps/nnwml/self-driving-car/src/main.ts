0ee56d09352c83262accf91299cb42521ef7d872
Lloyd Lobo <lloyd@lloydlobo.com>
2022-05-19 16:50:38 +0530

    docs: update comments and arrange code

--- apps/nnwml/self-driving-car/src/main.ts
+++ apps/nnwml/self-driving-car/src/main.ts
@@ -1,8 +1,6 @@
 import './app/app.element.ts';
 import './app/features/road';
 import { Car, Road } from './app';
-export const canvas = document.getElementById('myCanvas') as HTMLCanvasElement; // object
-canvas.width = 200;
 
 /**
 
@@ -15,19 +13,19 @@ canvas.width = 200;
 
   */
 
+export const canvas = document.getElementById('myCanvas') as HTMLCanvasElement; // object
+canvas.width = 200;
 // a drawing context is a way to draw on a canvas
 export const ctx: CanvasRenderingContext2D = canvas.getContext('2d');
 const road = new Road(
   (canvas.width / 2) as number,
   (canvas.width * 0.9) as number
 );
-// car dimensions in pixels (x,y,width,height) 0,0 is top left corner
-
+/* car dimensions in pixels (x,y,width,height) 0,0 is top left corner */
 /* getLaneCenter(1) puts the car in the middle => 0 , 1 , 2 */
 export const car: Car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
-// add car to DOM (in the canvas)
-car.draw(ctx);
+car.draw(ctx); /* draw car on the canvas in the DOM */
 
 animate();
 
@@ -40,6 +38,8 @@ export function animate() {
   /* calls the animate() method again and again gives the illusion of movement of the car */
   requestAnimationFrame(animate);
 }
+
+// ============================================================================
 
 /**
 
