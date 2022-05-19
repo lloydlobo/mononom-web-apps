f90560f48b2a2f8071099949b50d9eb826fe7d07
Lloyd Lobo <lloyd@lloydlobo.com>
2022-05-19 12:44:02 +0530

    feat: add linearInterpolation lerp() utility

--- apps/nnwml/self-driving-car/src/app/features/road.ts
+++ apps/nnwml/self-driving-car/src/app/features/road.ts
@@ -1,11 +1,11 @@
 export class Road {
-  x: number;
-  width: number;
+  bottom: number;
   laneCount: number;
   left: number;
   right: number;
   top: number;
-  bottom: number;
+  width: number;
+  x: number;
   constructor(x: number, width: number, laneCount = 3 as number) {
     this.x = x;
     this.width = width;
@@ -23,6 +23,14 @@ export class Road {
   draw(ctx: CanvasRenderingContext2D): void {
     ctx.lineWidth = 5 as number;
     ctx.strokeStyle = 'white' as string | CanvasGradient | CanvasPattern;
+    // add multiple lanes with a for loop
+    for (let i = 0; i < this.laneCount; i += 1) {
+      // we need to find the x coordinate of the lane lines to draw
+      /* use LINEAR INTERPOLATION or lerp
+      getting values from left to right depending on a percentage
+      i.e. is i/laneCount */
+      const x = lerp(this.left, this.right, i / this.laneCount);
+    }
     // draw line on left side of road
     ctx.beginPath();
     ctx.moveTo(this.left, this.top);
@@ -35,4 +43,20 @@ export class Road {
     ctx.stroke();
   } /* after this add in main.ts
   const road = new Road(canvas.width/2, canvas.width) */
+}
+
+/**
+ * Linear Interpolation between two values A and B based on the percentage t
+ * when t = 0, return A
+ * when t = 1, return B
+ * when t = 0.5, return the midpoint between A and B
+ * @date 5/19/2022 - 12:40:55 PM
+ *
+ * @param {number} A
+ * @param {number} B
+ * @param {number} t
+ * @returns {number}
+ */
+export function lerp(A: number, B: number, t: number): number {
+  return (A + (B - A) * t) as number;
 }
