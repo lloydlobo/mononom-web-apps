047485c8f4b51ae36569fcb74d095e0d2aa19731
lloydlobo <lloyd@lloydlobo.com>
2022-05-25 18:51:00 +0530

    fix: add border functionality slow perf

--- apps/nnwml/nascar/src/app/features/road.ts
+++ apps/nnwml/nascar/src/app/features/road.ts
@@ -3,16 +3,12 @@ import { lerp } from '../utils/';
 export class Road {
   left: number;
   right: number;
-  top: number;
-  bottom: number;
+  borders: { x: number; y: number; radius: number }[];
   constructor(public x: number, public width: number, public laneCount = 6) {
     this.x = x;
     this.width = width;
     this.laneCount = laneCount;
 
-    const infinity = 1000000;
-    this.top = -infinity;
-    this.bottom = infinity;
     // this.lanes = [];
     // for (let i = 0; i < this.laneCount; i++) {
     //   this.lanes.push(new Lane(this.x, this.y, this.width, i));
@@ -20,6 +16,15 @@ export class Road {
 
     this.left = x - width / 2;
     this.right = x + width / 2;
+
+    // draw border for circular lanes where the car can go in circle, this.borders is an array of objects
+    this.borders = [
+      {
+        x: this.left,
+        y: this.right,
+        radius: (this.right - this.left) / 2,
+      },
+    ];
   }
 
   /**
@@ -43,10 +48,9 @@ export class Road {
   draw(ctx) {
     ctx.lineWidth = 4;
     ctx.strokeStyle = '#fff';
-
-    for (let i = 0; i <= this.laneCount; i += 1) {
-      // what is the x coordinate of each of the lines/arcs? Use linear interpolation
+    for (let i = 1; i <= this.laneCount - 1; i += 1) {
       const x = lerp(this.left, this.right, i / this.laneCount);
+      // what is the x coordinate of each of the lines/arcs? Use linear interpolation
       // const y = lerp(this.right, this.left, i / this.laneCount);
       if (i > 3 && i < this.laneCount) {
         ctx.setLineDash([20, 20]);
@@ -55,17 +59,52 @@ export class Road {
       }
 
       ctx.beginPath();
-      ctx.lineWidth = 0;
       ctx.arc(this.width / 2, this.right / 2, x / 2, 0, Math.PI * 2);
+      ctx.stroke();
+      this.borders.push({
+        x: this.width,
+        y: this.right,
+        radius: x / 2,
+      });
+    }
 
+    ctx.beginPath();
+    for (let i = 0; i < this.laneCount; i++) {
+      this.borders[i].x = this.getLaneCenter(i);
+      this.borders[i].y = this.right;
+      ctx.strokeStyle = 'red';
+      ctx.arc(
+        this.borders[i].x,
+        this.borders[i].y,
+        this.borders[i].radius,
+        0,
+        Math.PI * 2
+      );
       ctx.stroke();
     }
+    // instead do it for borders[0] and borders[laneCount] -> forEach won't work but it can if it's for sensors!!!
+    // ctx.setLineDash([]);
+    // ctx.lineWidth = 4;
+    // ctx.strokeStyle = 'red';
+    // this.borders.forEach((border) => {
+    //   ctx.beginPath();
+    //   ctx.arc(this.width / 2, this.right / 2, border.radius, 0, Math.PI * 2);
+    //   ctx.stroke();
+    // });
   }
 }
 
 /**
  * ARCHIVE
  *
+ * @202205251845
+ *  ctx.lineWidth = 4;
+    ctx.strokeStyle = 'red';
+    this.borders.forEach((border) => {
+      ctx.beginPath();
+      ctx.arc(this.width / 2, this.right / 2, border.radius, 0, Math.PI * 2);
+      ctx.stroke();
+    });
  * @202205251541
  * ctx.arc(
   (this.left + this.right) / 2,
