4e7518821cfa6eb3d55470f8c66beb27d3a31042
lloydlobo <lloyd@lloydlobo.com>
2022-05-25 12:30:53 +0530

    feat: add angle to rotate the car & translate

--- apps/nnwml/nascar/src/app/features/car.ts
+++ apps/nnwml/nascar/src/app/features/car.ts
@@ -5,6 +5,7 @@ export class Car {
   acceleration: number;
   controls: Controls;
   friction: number;
+  angle: number;
 
   constructor(
     public x: number,
@@ -23,6 +24,8 @@ export class Car {
 
     this.maxSpeed = maxSpeed;
     this.friction = 0.05;
+
+    this.angle = 0; // implemented to curb car going past maxSpeed diagonally
 
     this.controls = new Controls();
   }
@@ -59,23 +62,60 @@ export class Car {
       this.speed = 0;
     }
 
+    /**
+     * implement basics first then advance later -- based on unit circle
+     * this will be the coordinate system for rotations
+     * in draw(ctx) save the context and translate to the point where you want the rotation to be centered at
+     * @param angle
+     * @see https://img1.wikia.nocookie.net/__cb20140408175825/math/en/images/8/87/Unit_circle_indentities.png (IMAGE)
+     * */
+    if (this.controls.left) {
+      // this.x -= 2;
+      this.angle += 0.03;
+    }
+    if (this.controls.right) {
+      this.angle -= 0.03;
+      // this.x += 2;
+    }
+
     // this.x -= this.speed;
     this.y -= this.speed;
   }
 
   draw(ctx: CanvasRenderingContext2D) {
-    // ctx.save();
-    // ctx.translate(this.x, this.y);
+    ctx.save();
+    ctx.translate(this.x, this.y); // then remove this.x - this.width / 2 etc from rect() as we are already translating it here
+    ctx.rotate(-this.angle);
 
     ctx.beginPath();
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     // x will be center
     ctx.fill();
-    // ctx.restore();
+
+    ctx.restore(); // after translating at each frame infinite series happen so restore it
   }
 }
+
+// Language: typescript
+//
+// ARCHIVE
+//
+
+// * 202205221228
+
+// draw(ctx: CanvasRenderingContext2D) {
+//   ctx.save();
+//   ctx.translate(this.x, this.y); // then remove this.x - this.width / 2 etc from rect() as we are already translating it here
+//   ctx.rotate(-this.angle);
+
+//   ctx.beginPath();
+//   ctx.rect(
+//     this.x - this.width / 2,
+//     this.y - this.height / 2,
+//     this.width,
+//     this.height
+//   );
+//   // x will be center
+//   ctx.fill();
+//   // ctx.restore();
+// }
