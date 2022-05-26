6516e469ef0bb9e8e05746d37cc292ab7d8290b4
lloydlobo <lloyd@lloydlobo.com>
2022-05-26 09:30:36 +0530

    feat: add frequency, animate bg & stroke

--- apps/waves/sine/src/main.ts
+++ apps/waves/sine/src/main.ts
@@ -11,23 +11,36 @@ canvas.height = window.innerHeight;
 
 const wave = {
   y: canvas.height / 2,
-  length: 0.01,
+  wavelength: 0.01,
   amplitude: 100,
+  frequency: 0.01,
 };
 
 gui.add(wave, 'y', 0, canvas.height);
-gui.add(wave, 'length', -0.01, 0.01);
+gui.add(wave, 'wavelength', -0.01, 0.01);
 gui.add(wave, 'amplitude', -300, 300);
+gui.add(wave, 'frequency', -0.01, 1);
 
+let increment = wave.frequency;
 function animate() {
   requestAnimationFrame(animate);
+  ctx.fillStyle = `rgba(0, 0, 0, 0.01)`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  // ctx.clearRect(0, 0, canvas.width, canvas.height);
+
   ctx.beginPath();
   ctx.moveTo(0, canvas.height / 2);
   for (let i = 0; i < canvas.width; i += 1) {
-    ctx.lineTo(i, wave.y + Math.sin(i * wave.length) * wave.amplitude); // draw a line each time we iterate through the for loop // Math.sin() returns -1 to +1
+    ctx.lineTo(
+      i,
+      wave.y + Math.sin(i * wave.wavelength + increment) * wave.amplitude
+    ); // draw a line each time we iterate through the for loop // Math.sin() returns -1 to +1
   } // create a point for each pixel, right now we have two controllable points the left and right ends of window - least 600 points is enough
-
+  // ctx.strokeStyle = `hsl(${Math.random() * 360}, 50%, 50%)`;
+  ctx.strokeStyle = `hsl(0, 50%, 50%)`;
   ctx.stroke();
+
+  increment += wave.frequency;
 }
 
 animate();
