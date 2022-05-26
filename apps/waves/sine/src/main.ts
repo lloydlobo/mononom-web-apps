017f453e1a5155b64d98a15148ab739e01a00dd2
lloydlobo <lloyd@lloydlobo.com>
2022-05-26 09:08:45 +0530

    feat: add animate() to requestAnimationFrame recursively

--- apps/waves/sine/src/main.ts
+++ apps/waves/sine/src/main.ts
@@ -19,15 +19,20 @@ gui.add(wave, 'y', 0, canvas.height);
 gui.add(wave, 'length', -0.01, 0.01);
 gui.add(wave, 'amplitude', -300, 300);
 
-ctx.beginPath();
-ctx.moveTo(0, canvas.height / 2);
-for (let i = 0; i < canvas.width; i += 1) {
-  const amplitude = 100;
-  const waveLength = 0.01;
-  ctx.lineTo(i, canvas.height / 2 + Math.sin(i * waveLength) * amplitude); // draw a line each time we iterate through the for loop // Math.sin() returns -1 to +1
-} // create a point for each pixel, right now we have two controllable points the left and right ends of window - least 600 points is enough
+function animate() {
+  requestAnimationFrame(animate);
+  ctx.beginPath();
+  ctx.moveTo(0, canvas.height / 2);
+  for (let i = 0; i < canvas.width; i += 1) {
+    const amplitude = 100;
+    const waveLength = 0.01;
+    ctx.lineTo(i, canvas.height / 2 + Math.sin(i * waveLength) * amplitude); // draw a line each time we iterate through the for loop // Math.sin() returns -1 to +1
+  } // create a point for each pixel, right now we have two controllable points the left and right ends of window - least 600 points is enough
 
-ctx.stroke();
+  ctx.stroke();
+}
+
+animate();
 
 /**
  * Archive
